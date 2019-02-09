3dc5f607d7b6c566395c0ceec638b79bc64ebc0a
roossh <roossh@users.noreply.github.com>
2019-02-09 12:47:06 +0200

    Update index.js

--- index.js
+++ index.js
@@ -102,7 +102,7 @@ app.post('/api/persons', (req, res) => {
     res.json(personObject)
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
